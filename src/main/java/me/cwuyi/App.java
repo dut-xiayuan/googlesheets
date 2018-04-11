cf204db0e19906fec2fd411acd299902e9a0234a
cwuyi <cwuyi@cwuyi.local>
2018-04-11 11:11:31 +0800

    insert method added

--- src/main/java/me/cwuyi/App.java
+++ src/main/java/me/cwuyi/App.java
@@ -1,10 +1,26 @@
 package me.cwuyi;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args ) { System.out.println( "Hello World!" );}
+public class App {
+    public static void main( String[] args ) throws Exception {
+
+        String spreadsheetId = "1X1ptb1B57wsL42fv45mme22YAAZy-8z5G9yopzSoS1M";
+        String range = "sheet1!A3:D4";
+
+        List<String> list1 = Arrays.asList("3A", "3B", "3C", "3D");
+        List<String> list2 = Arrays.asList("4A", "4B", "4", "4");
+        List<List<Object>> list = new ArrayList<>();
+        list.add(new ArrayList<Object>(list1));
+        list.add(new ArrayList<Object>(list2));
+
+        GoogleSheetsUtil.insert(spreadsheetId, range, list);
+
+    }
 }
---
+++ src/main/java/me/cwuyi/GoogleSheetsUtil.java
@@ -0,0 +1,122 @@
+package me.cwuyi;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.UpdateValuesResponse;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+
+public class GoogleSheetsUtil {
+
+    /** Application name. */
+    private static final String APPLICATION_NAME =
+            "Google Sheets API Java Quickstart";
+
+    /** Directory to store user credentials for this application. */
+    private static final java.io.File DATA_STORE_DIR = new java.io.File(
+            System.getProperty("user.home"), ".credentials/sheets.googleapis.com");
+
+    /** Global instance of the {@link FileDataStoreFactory}. */
+    private static FileDataStoreFactory DATA_STORE_FACTORY;
+
+    /** Global instance of the JSON factory. */
+    private static final JsonFactory JSON_FACTORY =
+            JacksonFactory.getDefaultInstance();
+
+    /** Global instance of the HTTP transport. */
+    private static HttpTransport HTTP_TRANSPORT;
+
+    /** Global instance of the scopes required by this quickstart.
+     *
+     * If modifying these scopes, delete your previously saved credentials
+     * at ~/.credentials/sheets.googleapis.com-java-quickstart
+     */
+    private static final List<String> SCOPES =
+            Arrays.asList(SheetsScopes.SPREADSHEETS);
+
+    static {
+        try {
+            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
+        } catch (Throwable t) {
+            t.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+
+    /**
+     * Creates an authorized Credential object.
+     * @return an authorized Credential object.
+     * @throws IOException
+     */
+    private static Credential authorize() throws IOException {
+        // Load client secrets.
+        InputStream in =
+                GoogleSheetsUtil.class.getResourceAsStream("/client_secret.json");
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow =
+                new GoogleAuthorizationCodeFlow.Builder(
+                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                        .setDataStoreFactory(DATA_STORE_FACTORY)
+                        .setAccessType("offline")
+                        .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver()).authorize("xiayuan1992@gmail.com");
+        System.out.println(
+                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
+        return credential;
+    }
+
+    /**
+     * Build and return an authorized Sheets API client service.
+     * @return an authorized Sheets API client service
+     * @throws IOException
+     */
+    private static Sheets getSheetsService() throws IOException {
+        Credential credential = authorize();
+        return new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
+
+    /**
+     *
+     * @param spreadsheetId 表单id
+     * @param range 插入表单的范围，形如：sheet1!A3:D4，如果范围与已有数据重合，会被覆盖
+     * @param insertValues 插入的值，是一个两层的list，比如[[1,2,3,4],[1,2,3,4]]
+     * @return
+     * @throws Exception
+     */
+
+    public static UpdateValuesResponse insert(String spreadsheetId, String range, List<List<Object>> insertValues) throws Exception {
+        Sheets service = getSheetsService();
+
+        ValueRange body = new ValueRange().setValues(insertValues);
+        UpdateValuesResponse result = service.spreadsheets().values().update(spreadsheetId, range, body)
+                        .setValueInputOption("RAW")
+                        .execute();
+
+        return result;
+    }
+
+
+}
